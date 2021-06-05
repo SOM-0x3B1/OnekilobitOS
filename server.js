a7a6638ddff18e2bb738e80dd4b20f64c3d07aa4
Onekilobit <somadagaszkar@gmail.com>
2021-06-05 15:36:19 +0200

    Add basic chat functions to backend

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const express = require('express');
-//const favicon = require('serve-favicon');
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const { joinUser, removeUser, findUser } = require('./users');
 
-const path = require('path');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
-const app = express();
-
-
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
-})
-
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/' + req.path))
-})
+let thisRoom = "";
 
 
-app.listen(9000);
+io.on("connection", function (socket) {
+    console.log("connected");
+
+    socket.on("join room", (data) => {
+        console.log('in room');
+        let Newuser = joinUser(socket.id, data.username, data.roomName)
+
+        socket.emit('send data', { id: socket.id, username: Newuser.username, roomname: Newuser.roomname });
+
+        thisRoom = Newuser.roomname;
+        console.log(Newuser);
+        socket.join(Newuser.roomname);
+    });
+
+    socket.on("chat message", (data) => {
+        io.to(thisRoom).emit("chat message", { data: data, id: socket.id });
+    });
+
+    socket.on("disconnect", () => {
+        const user = removeUser(socket.id);
+        console.log(user);        
+        if (user) {
+            console.log(user.username + ' has left');
+        }
+        console.log("disconnected");
+    });
+});
+
+
+http.listen(9000, function () { });
