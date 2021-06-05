19dd6ffc2a18a6298b2ae5b8062d44c12ed50fbf
Onekilobit <somadagaszkar@gmail.com>
2021-06-05 17:19:35 +0200

    Add frontend messenger without design

--- public/sound.js
+++ public/sound.js
@@ -1,7 +1,7 @@
-var fan = new Audio('fan.mp3');
-var startup = new Audio('startup.mp3');
-var beep = new Audio('beep.mp3');
-var enter = new Audio('enter.mp3');
-var arrows = new Audio('arrows.mp3');
+var fan = new Audio('media/fan.mp3');
+var startup = new Audio('media/startup.mp3');
+var beep = new Audio('media/beep.mp3');
+var enter = new Audio('media/enter.mp3');
+var arrows = new Audio('media/arrows.mp3');
 
 fan.loop = true;
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const app = require("express")();
+const express = require('express');
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const { joinUser, removeUser, findUser } = require('./users');
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
+app.use(express.static('public'));
 
 let thisRoom = "";
 
