60604233d877f2e0f510db21a224f8547ca1aa34
Onekilobit <somadagaszkar@gmail.com>
2021-06-06 19:24:50 +0200

    Add faulty https

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const fs = require('fs');
+const path = require('path')
 const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.readFileSync('cert/_.onekilobit.eu-key.pem', 'utf8');
+const certificate = fs.readFileSync('cert/_.onekilobit.eu-crt.pem', 'utf8');
+
 const { joinUser, removeUser, findUser } = require('./users');
 const cookieParser = require('cookie-parser');
+
+const credentials = { key: privateKey, cert: certificate };
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { Location: 'https://' + req.headers.host + req.url });
+    res.end();
+});
+const httpsServer = https.createServer(credentials, app);
+const io = require("socket.io")(httpsServer);
 
 app.use(cookieParser());
 
@@ -21,21 +35,29 @@ app.use(cookieParser());
     res.send();
 });*/
 
-app.use(express.static('public'));
+app.all('*', function (req, res, next) {    
+    var host = req.header("host");
+    if (req.subdomains[0]) {
+        next();
+    } else {
+        res.redirect(301, "https://www." + host);
+    }
+});
+
 
 app.get('/', function (req, res) {
-    console.log(req.cookies);
+    if(req.subdomains[0]==='www')
+        res.sendFile(path.join(__dirname + '/public-main/main.html'))
+    else if(req.subdomains[0]==='os')
+        res.sendFile(path.join(__dirname + '/public-os/index.html'));
+})
 
-    if (!req.cookies.latogatott) {
-        res.cookie('latogatott', '0');
-    }
-    else {
-        let lat = parseInt(req.cookies.latogatott) + 1;
-        res.cookie('latogatott', lat.toString());
-    }
-
-    res.send();
-});
+app.get('*', function (req, res) {
+    if(req.subdomains[0]==='www')
+        res.sendFile(path.join(__dirname + '/public-main/' + req.path))
+    else if(req.subdomains[0]==='os')
+        res.sendFile(path.join(__dirname + '/public-os/' + req.path))
+})
 
 
 let thisRoom = "";
@@ -60,7 +82,7 @@ io.on("connection", function (socket) {
 
     socket.on("disconnect", () => {
         const user = removeUser(socket.id);
-        console.log(user);        
+        console.log(user);
         if (user) {
             console.log(user.username + ' has left');
         }
@@ -69,4 +91,5 @@ io.on("connection", function (socket) {
 });
 
 
-http.listen(9000, function () { });
+httpServer.listen(80, function () { });
+httpsServer.listen(443, function () { });
